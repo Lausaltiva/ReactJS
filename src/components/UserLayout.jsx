8c289b75374850a39bc722f34f1f77f6ab86d4f4
Lautaro_Saltiva <lautarosaltiva2021@gmail.com>
2022-11-10 12:51:52 -0300

    segunda-entrega-8

--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -1,5 +1,5 @@
 import  Navbar from "./Navbar";
-import Footer from "./footer";
+import Footer from "./Footer";
 import "../styles/App.scss";
 export const UserLayout = ({ children }) => {
     return (
