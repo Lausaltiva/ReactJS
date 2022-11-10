461469db9dc0c8f462cd7cd9256b777562d34e5e
Lautaro_Saltiva <lautarosaltiva2021@gmail.com>
2022-11-10 12:37:46 -0300

    segunda entrega-4

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
 
-    return <footer className="footer">
+    return <Footer className="footer">
         <h3 className="footer__item" >Politica de privacidad</h3>
         <h3 className="footer__item" >Terminos y condiciones</h3>
         <h3 className="footer__item" >Contacto</h3>
-    </footer>
+    </Footer>
 }
 
 export default Footer
